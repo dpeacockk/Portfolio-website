34ba729dfa81308bd24600919b5216508c009c1e
Daniel Peacock <dpeacock@umich.edu>
2024-03-10 21:57:29 -0500

    added footer

--- with-tailwindcss-app/app/page.tsx
+++ with-tailwindcss-app/app/page.tsx
@@ -5,19 +5,19 @@ export default function Home() {
     <main className="min-h-screen">
       <div className="navbar-wrapper">
         <ul className="navbar">
-          <li className="navbar_element">Home</li>
-          <li className="navbar_element">Education</li>
-          <li className="navbar_element">Experience</li>
-          <li className="navbar_element">Projects</li>
-          <li className="navbar_element">For Fun</li>
-          <li className="navbar_element">Contact</li>
+          <li className="navbar-element">Home</li>
+          <li className="navbar-element">Education</li>
+          <li className="navbar-element">Experience</li>
+          <li className="navbar-element">Projects</li>
+          <li className="navbar-element">For Fun</li>
+          <li className="navbar-element">Contact</li>
         </ul>
       </div>
       <div className="home-wrapper">
         HELLO WORLD
       </div>
       <div className="footer-wrapper">
-        
+        cocks
       </div>
     </main>
   );
